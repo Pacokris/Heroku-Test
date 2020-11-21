90efaeb556ea52096194ebdb7260149902e44b28
Franck <pacokris@yahoo.fr>
2020-11-21 14:36:09 +0100

    java version

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -3,11 +3,16 @@ package com.example.helloworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Properties;
+
 @SpringBootApplication
 public class HelloworldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
+		Properties pros = System.getProperties();
+		pros.setProperty( "java.version", "11" );
+		pros.list(System.out);
 	}
 
 }
