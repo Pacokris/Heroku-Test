4a3af9500f244330310e90af5eda23d7dfccfab1
Franck <pacokris@yahoo.fr>
2020-11-21 15:54:11 +0100

    update

--- src/main/java/com/example/helloworld/controller/HomeController.java
+++ src/main/java/com/example/helloworld/controller/HomeController.java
@@ -1,12 +1,17 @@
 package com.example.helloworld.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.time.Instant;
 import java.util.Map;
 
-@Controller
+/*@Controller
 public class HomeController {
 
     private static final String HOME = "/home";
@@ -17,5 +22,19 @@ public class HomeController {
         model.put( "helloWorld", helloWorld );
 
         return new ModelAndView( HOME, model );
+    }
+}*/
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    private static final String HOME = "/home";
+
+    @GetMapping("/")
+    @ResponseStatus(HttpStatus.OK)
+    public String getCurrentTime() {
+
+        return Instant.now().toString();
     }
 }
